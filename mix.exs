8549b74ac0532fa0c893daf305fbe2d9b610bdea
Gabriel Jaldon <gjaldon85@gmail.com>
2015-10-20 02:53:36 +0800

    Support Elixir 1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mariaex.Mixfile do
   def project do
     [app: :mariaex,
      version: "0.5.0",
-     elixir: "~> 1.1",
+     elixir: "~> 1.0",
      deps: deps,
      name: "Mariaex",
      source_url: "https://github.com/liveforeverx/mariaex",
