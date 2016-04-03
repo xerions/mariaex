ee4e830d4e537c6bb83f00e047bdb6da4a018507
Dmitry Russ <dmitry.a.russ@gmail.com>
2016-04-03 12:49:18 +0200

    Format connection error in exceptions

--- lib/mariaex/structs.ex
+++ lib/mariaex/structs.ex
@@ -22,13 +22,16 @@ defmodule Mariaex.Result do
 end
 
 defmodule Mariaex.Error do
-  defexception [:message, :mariadb, :connection_id]
+  defexception [:message, :tag, :action, :reason, :mariadb, :connection_id]
 
   def message(e) do
-    if kw = e.mariadb do
-      "(#{kw[:code]}): #{kw[:message]}"
-    else
-      e.message || ""
+    cond do
+      kw = e.mariadb ->
+        "(#{kw[:code]}): #{kw[:message]}"
+      tag = e.tag ->
+        "[#{tag}] `#{e.action}` failed with: #{inspect e.reason}"
+      true ->
+        e.message || ""
     end
   end
 end
