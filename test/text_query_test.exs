5bdc275119663ece572f4f26c47245ea053e2de8
johnxia <john.xia@teacherspayteachers.com>
2016-09-27 11:04:45 -0400

    Fix bug with returning rows as empty list instead of nil
    * Fix tablename overlap between 'test' and 'test'

--- test/text_query_test.exs
+++ test/text_query_test.exs
@@ -2,13 +2,13 @@ defmodule TextQueryTest do
   use ExUnit.Case, async: true
   import Mariaex.TestHelper
 
-  setup do
+  setup_all do
     opts = [database: "mariaex_test", username: "mariaex_user", password: "mariaex_pass", backoff_type: :stop]
     {:ok, pid} = Mariaex.Connection.start_link(opts)
-    drop = "DROP TABLE IF EXISTS test"
-    {:ok, _} = Mariaex.execute(pid, %Mariaex.Query{type: :text, statement: drop}, [])
+    # drop = "DROP TABLE IF EXISTS test"
+    # {:ok, _} = Mariaex.execute(pid, %Mariaex.Query{type: :text, statement: drop}, [])
     create = """
-    CREATE TABLE test (
+    CREATE TABLE test_text_query_table (
     id serial,
     bools boolean,
     bits bit(2),
@@ -21,7 +21,7 @@ defmodule TextQueryTest do
     """
     {:ok, _} = Mariaex.execute(pid, %Mariaex.Query{type: :text, statement: create}, [])
     insert = """
-    INSERT INTO test (id, bools, bits, varchars, texts, floats, ts, dt)
+    INSERT INTO test_text_query_table (id, bools, bits, varchars, texts, floats, ts, dt)
     VALUES
     (1, true, b'10', 'hello', 'world', 1.1, '2016-09-26 16:36:06', '0001-01-01 00:00:00'),
     (2, false, b'11', 'goodbye', 'earth', 1.2, '2016-09-26T16:36:07', '0001-01-01 00:00:01')
@@ -31,43 +31,43 @@ defmodule TextQueryTest do
   end
 
   test "select int", context do
-    rows = execute_text("SELECT id FROM test", [])
+    rows = execute_text("SELECT id FROM test_text_query_table", [])
     assert(rows == [[1], [2]])
   end
 
   test "select bool", context do
     # bool is tinyint
-    rows = execute_text("SELECT bools FROM test", [])
+    rows = execute_text("SELECT bools FROM test_text_query_table", [])
     assert(rows == [[1], [0]])
   end
 
   test "select bits", context do
-    rows = execute_text("SELECT bits FROM test", [])
+    rows = execute_text("SELECT bits FROM test_text_query_table", [])
     assert(rows == [[<<2>>], [<<3>>]])
   end
 
   test "select string", context do
-    rows = execute_text("SELECT floats FROM test", [])
+    rows = execute_text("SELECT floats FROM test_text_query_table", [])
     assert(rows == [[1.1], [1.2]])
   end
 
   test "select float", context do
-    rows = execute_text("SELECT floats FROM test", [])
+    rows = execute_text("SELECT floats FROM test_text_query_table", [])
     assert(rows == [[1.1], [1.2]])
   end
 
   test "select timestamp", context do
-    rows = execute_text("SELECT ts FROM test", [])
+    rows = execute_text("SELECT ts FROM test_text_query_table", [])
     assert(rows == [[{{2016, 9, 26}, {16, 36, 06, 0}}], [{{2016, 9, 26}, {16, 36, 07, 0}}]])
   end
 
   test "select datetime", context do
-    rows = execute_text("SELECT dt FROM test", [])
+    rows = execute_text("SELECT dt FROM test_text_query_table", [])
     assert(rows == [[{{1,1,1}, {0,0,0,0}}], [{{1,1,1}, {0,0,1,0}}]])
   end
 
   test "select multiple columns", context do
-    rows = execute_text("SELECT id, varchars FROM test", [])
+    rows = execute_text("SELECT id, varchars FROM test_text_query_table", [])
     assert(rows == [[1, "hello"], [2, "goodbye"]])
   end
 
