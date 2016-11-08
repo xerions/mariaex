eee73801df2b5ee437a3cc1d839ae1807daab2ef
James Fish <james@fishcakez.com>
2016-11-08 17:44:24 +0000

    Add basic stream tests

---
+++ test/stream_test.exs
@@ -0,0 +1,95 @@
+defmodule StreamTest do
+  use ExUnit.Case, async: true
+  import Mariaex.TestHelper
+
+  setup_all do
+    {:ok, pid} = connect()
+    {:ok, _} = Mariaex.query(pid, "CREATE TABLE stream (id int, text text)", [])
+    {:ok, _} = Mariaex.query(pid, "INSERT INTO stream VALUES (1, 'foo'), (2, 'bar')", [])
+    :sys.terminate(pid, :normal)
+    :ok
+  end
+
+  setup do
+    {:ok, pid} = connect()
+    {:ok, [pid: pid]}
+  end
+
+  test "simple text stream", context do
+    assert Mariaex.transaction(context[:pid], fn(conn) ->
+      stream = Mariaex.stream(conn, "SELECT * FROM stream", [], [])
+      assert [%Mariaex.Result{num_rows: 0, rows: []},
+              %Mariaex.Result{num_rows: 2, rows: [[1, "foo"], [2, "bar"]]}] =
+        Enum.to_list(stream)
+      :done
+    end) == {:ok, :done}
+  end
+
+  test "simple unnamed unprepared stream", context do
+    assert Mariaex.transaction(context[:pid], fn(conn) ->
+      stream = Mariaex.stream(conn, "SELECT * FROM stream", [], [])
+      assert [%Mariaex.Result{num_rows: 0, rows: []},
+              %Mariaex.Result{num_rows: 2, rows: [[1, "foo"], [2, "bar"]]}] =
+        Enum.to_list(stream)
+      :done
+    end) == {:ok, :done}
+    assert [[1, "foo"], [2, "bar"]] = query("SELECT * FROM stream", [])
+  end
+
+  test "simple unnamed prepared stream", context do
+    query = prepare("", "SELECT * FROM stream")
+    assert Mariaex.transaction(context[:pid], fn(conn) ->
+      stream = Mariaex.stream(conn, query, [], [])
+      assert [%Mariaex.Result{num_rows: 0, rows: []},
+              %Mariaex.Result{num_rows: 2, rows: [[1, "foo"], [2, "bar"]]}] =
+        Enum.to_list(stream)
+      :done
+    end) == {:ok, :done}
+    assert [[1, "foo"], [2, "bar"]] = execute(query, [])
+  end
+
+  test "simple named prepared stream", context do
+    query = prepare("stream", "SELECT * FROM stream")
+    assert Mariaex.transaction(context[:pid], fn(conn) ->
+      stream = DBConnection.stream(conn, query, [], [])
+      assert [%Mariaex.Result{num_rows: 0, rows: []},
+              %Mariaex.Result{num_rows: 2, rows: [[1, "foo"], [2, "bar"]]}] =
+        Enum.to_list(stream)
+      :done
+    end) == {:ok, :done}
+    assert [[1, "foo"], [2, "bar"]] = execute(query, [])
+  end
+
+  test "interleaving unnamed prepared stream", context do
+    query = prepare("", "SELECT * FROM stream")
+    assert Mariaex.transaction(context[:pid], fn(conn) ->
+      stream = Mariaex.stream(conn, query, [], [])
+      assert [{%Mariaex.Result{num_rows: 0, rows: []},
+               %Mariaex.Result{num_rows: 0, rows: []}},
+              {%Mariaex.Result{num_rows: 2, rows: [[1, "foo"], [2, "bar"]]},
+               %Mariaex.Result{num_rows: 2, rows: [[1, "foo"], [2, "bar"]]}}] =
+        Enum.zip(stream, stream)
+      :done
+    end) == {:ok, :done}
+    assert [[1, "foo"], [2, "bar"]] = execute(query, [])
+  end
+
+  test "interleaving named prepared stream", context do
+    query = prepare("stream", "SELECT * FROM stream")
+    assert Mariaex.transaction(context[:pid], fn(conn) ->
+      stream = Mariaex.stream(conn, query, [], [])
+      assert [{%Mariaex.Result{num_rows: 0, rows: []},
+               %Mariaex.Result{num_rows: 0, rows: []}},
+              {%Mariaex.Result{num_rows: 2, rows: [[1, "foo"], [2, "bar"]]},
+               %Mariaex.Result{num_rows: 2, rows: [[1, "foo"], [2, "bar"]]}}] =
+        Enum.zip(stream, stream)
+      :done
+    end) == {:ok, :done}
+    assert [[1, "foo"], [2, "bar"]] = execute(query, [])
+  end
+
+  defp connect() do
+    opts = [database: "mariaex_test", username: "mariaex_user", password: "mariaex_pass", backoff_type: :stop]
+    Mariaex.Connection.start_link(opts)
+  end
+end
