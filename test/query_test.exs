56d339d81fdab65beb697e4741a5537b8e108626
James Fish <james@fishcakez.com>
2016-12-05 19:15:07 +0000

    Test LRU cache gc

--- test/query_test.exs
+++ test/query_test.exs
@@ -3,7 +3,7 @@ defmodule QueryTest do
   import Mariaex.TestHelper
 
   setup do
-    opts = [database: "mariaex_test", username: "mariaex_user", password: "mariaex_pass", backoff_type: :stop]
+    opts = [database: "mariaex_test", username: "mariaex_user", password: "mariaex_pass", cache_size: 2, backoff_type: :stop]
     {:ok, pid} = Mariaex.Connection.start_link(opts)
     {:ok, [pid: pid]}
   end
