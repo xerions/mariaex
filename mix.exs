e436a129ea9b97a2be66c030635a59fc43f1de56
Dmitry Russ <dmitry.a.russ@gmail.com>
2016-12-08 21:19:24 +0100

    Add ex_doc to dependencies

--- mix.exs
+++ mix.exs
@@ -21,7 +21,8 @@ defmodule Mariaex.Mixfile do
   defp deps do
     [{:decimal, "~> 1.0"},
      {:db_connection, "~> 1.1"},
-     {:coverex, "~> 1.4.10", only: :test}]
+     {:coverex, "~> 1.4.10", only: :test},
+     {:ex_doc, ">= 0.0.0", only: :dev}]
   end
 
   defp description do
