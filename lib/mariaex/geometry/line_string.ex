c17479d269403705918898246740af9e54d9be50
Sam Davies <sampdavies@gmail.com>
2017-07-12 11:07:52 +0100

    Select implemented for Polygon

--- lib/mariaex/geometry/line_string.ex
+++ lib/mariaex/geometry/line_string.ex
@@ -1,8 +1,8 @@
 defmodule Mariaex.Geometry.LineString do
   @moduledoc """
-  Defines the LineString struct
+  Define the LineString struct
   """
 
-  @type t :: %Mariaex.Geometry.LineString{coordinates: [{number, number}], srid: non_neg_integer | nil}
+  @type t :: %Mariaex.Geometry.LineString{ coordinates: [{number, number}], srid: non_neg_integer | nil }
   defstruct coordinates: [], srid: nil
 end
