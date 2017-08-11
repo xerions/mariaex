dc2268d07b9f616616a8a2bc5a19ce3408875231
Yury Gargay <yury.gargay@gmail.com>
2017-08-11 17:01:45 +0700

    Release v0.8.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Mariaex.Mixfile do
 
   def project do
     [app: :mariaex,
-     version: "0.8.2",
+     version: "0.8.3",
      elixir: "~> 1.2",
      deps: deps(),
      name: "Mariaex",
