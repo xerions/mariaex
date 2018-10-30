cfe38ae3829021106ee5def71593e0306aa1437f
Paul Engel <pm_engel@icloud.com>
2018-10-30 12:35:27 +0100

    Revert (for the prepared statements) irrelevant changes

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -56,10 +56,17 @@ sql = """
   DROP TABLE test1;
 """
 
+create_user =
+  case System.get_env("DB") do
+    "mysql:5.6" -> "CREATE USER"
+    _ -> "CREATE USER IF NOT EXISTS"
+  end
+
 cmds = [
   ~s(mysql #{mysql_connect} -e "DROP DATABASE IF EXISTS mariaex_test;"),
   ~s(mysql #{mysql_connect} -e "CREATE DATABASE mariaex_test DEFAULT CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';"),
-  ~s(mysql #{mysql_connect} -e "GRANT ALL ON *.* TO 'mariaex_user'@'%' IDENTIFIED BY 'mariaex_pass' WITH GRANT OPTION;"),
+  ~s(mysql #{mysql_connect} -e "#{create_user} 'mariaex_user'@'%' IDENTIFIED BY 'mariaex_pass';"),
+  ~s(mysql #{mysql_connect} -e "GRANT ALL ON *.* TO 'mariaex_user'@'%' WITH GRANT OPTION"),
   ~s(mysql --host=#{mysql_host} --port=#{mysql_port} --protocol=tcp -u mariaex_user -pmariaex_pass mariaex_test -e "#{
     sql
   }")
@@ -67,6 +74,7 @@ cmds = [
 
 Enum.each(cmds, fn cmd ->
   {status, output} = run_cmd.(cmd)
+  IO.puts("--> #{output}")
 
   if status != 0 do
     IO.puts("""
